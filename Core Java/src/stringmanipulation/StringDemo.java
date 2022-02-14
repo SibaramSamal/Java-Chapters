e0a34b3e6532b6fec28a1725530a4d47eada3801
Sibaram <sibaramsamal04@gmail.com>
2022-02-14 23:59:16 +0530

    String Manipulation initial commit

---
+++ Core Java/src/stringmanipulation/StringDemo.java
@@ -0,0 +1,12 @@
+package stringmanipulation;
+
+public class StringDemo {
+	private static String name = "Sibaram Samal";
+	private static String clg = new String("NIST");
+	private static StringBuffer sbr = new StringBuffer("String Buffer contents");
+	public static void main(String[] args) {
+		System.out.println(name);
+		System.out.println(clg);
+		System.out.println(sbr);
+	}
+}
