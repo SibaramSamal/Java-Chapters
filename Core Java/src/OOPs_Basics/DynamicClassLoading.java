14575a9bb3b29aa9a0337c1171ad84fab849b970
Sibaram <sibaramsamal04@gmail.com>
2022-04-02 20:50:03 +0530

    Dynamic class loading enhancements

--- Core Java/src/OOPs_Basics/DynamicClassLoading.java
+++ Core Java/src/OOPs_Basics/DynamicClassLoading.java
@@ -1,5 +1,7 @@
 package OOPs_Basics;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 class Sample{
@@ -15,10 +17,18 @@ class Sample{
 }
 
 public class DynamicClassLoading {
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		System.out.println("Enter class you want to load: ");
 		Scanner sc = new Scanner(System.in);
-		Class obj = Class.forName(sc.next());
-		obj.newInstance();
+		Class obj = Class.forName(sc.next());// for loading the class
+		/* After loading the class to create object, use newInstance(). But in this way, only default
+		   constructor in called. To call all other types of constructors, we have to collect all the 
+		   constructor references in an Constructor array and through the index number and parameterized
+		    newInstance(), we can call our required constructors
+		 */
+		obj.newInstance();	//For 0-Param constructor
+		Constructor[] ob = obj.getDeclaredConstructors();
+		ob[0].newInstance();	//For 0-Param constructor
+		ob[1].newInstance(5,6); //For 2-Param constructor
 	}
 }
