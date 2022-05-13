78fd5c9c710177ce6d254d110f1e9a7f53dd0d4a
Sibaram <sibaramsamal04@gmail.com>
2022-05-13 23:22:04 +0530

    File I/o reading and writing in file

---
+++ Core Java/src/file/io/InputStreamDemo.java
@@ -0,0 +1,37 @@
+package file.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class InputStreamDemo {
+	
+	public static void main(String[] args) {
+		try {
+			FileOutputStream obj = new FileOutputStream("C:\\Users\\Sibaram\\Desktop\\FileDemo.txt",true);
+			String data = "This is the demo contents to be writen in a file... ok";
+			byte[] byteConvertedData = data.getBytes();
+			for(int i=0 ; i<data.length() ; i++) {
+				obj.write(byteConvertedData[i]);
+			}
+			obj.write(97); // we can pass within -128 to 127
+			System.out.println("Data inserted successfully !!!");
+			
+			// to read file data
+			FileInputStream fin = new FileInputStream("C:\\Users\\Sibaram\\Desktop\\FileDemo.txt");
+			while(true) {
+				int i = fin.read();
+				if(i == -1) {
+					System.out.println("End of file");
+					break;
+				}
+				System.out.print((char)i);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
