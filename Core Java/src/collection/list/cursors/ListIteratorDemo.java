756c1ff355fff73629b63211c00de31c6e4ec6d4
Sibaram <sibaramsamal04@gmail.com>
2022-05-18 21:33:11 +0530

    cursors in collection

--- Core Java/src/collection/list/cursors/ListIteratorDemo.java
+++ Core Java/src/collection/list/cursors/ListIteratorDemo.java
@@ -6,20 +6,20 @@ import java.util.Vector;
 public class ListIteratorDemo {
 	public static void main(String[] args) {
 		Vector v = new Vector();
-		ListIterator l = v.listIterator();
 		v.addElement("Venky");
 		v.add("Balakrishna");
 		v.add("chiru");
 		v.add("nag");
+		ListIterator l = v.listIterator();
 		while(l.hasNext()) {
 			String nm = (String)l.next();
-			if(nm.equals("Balakrushna"))
+			if(nm.equals("Balakrishna"))
 				l.remove();
 			else if(nm.equals("chiru"))
 				l.set("Chiranjivi");
 			else if(nm.equals("nag"))
 				l.add(null);
 		}
-		System.out.println(l);
+		System.out.println(v); //[ Venky,Chiranjivi,nag,null ]
 	}
 }
