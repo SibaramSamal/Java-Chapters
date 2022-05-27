cbcddb42baac820103b3f01bf6d8a374a7c2b2ae
Sibaram <sibaramsamal04@gmail.com>
2022-05-27 22:40:48 +0530

    All Methods of HashMap

--- Core Java/src/collection/map/MapDemo.java
+++ Core Java/src/collection/map/MapDemo.java
@@ -1,6 +1,10 @@
 package collection.map;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class MapDemo {
 
@@ -12,6 +16,23 @@ public class MapDemo {
 		
 		System.out.println(m);	//{key=value, key=value,...}
 		System.out.println(m.put("Sibaram", 105));	//returns old value
+		
+		Set st = m.keySet();
+		System.out.println(st); // collection view of Map object will display keys of each entry
+		
+		Collection c = m.values();
+		System.out.println(c); // collection view of Map object will display values of each entry
+		
+		Set s = m.entrySet();
+		// logic to display and change "Entry" data of a map object 
+		Iterator iter = s.iterator();
+		while(iter.hasNext()) {
+			Map.Entry ent = (Map.Entry)iter.next();
+			System.out.println(ent.getKey()+" "+ent.getValue());
+			if(ent.getKey().equals("Sibaram"))
+					ent.setValue(10000);
+		}
+		System.out.println(m);
 	}
 
 }
